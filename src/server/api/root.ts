b293078a5443572d63c79d7d474ff187a90fd7f9
PedroHenriqueMG <pedrohdefsouza@gmail.com>
2024-01-09 23:31:20 -0300

    fix: build erros

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { postRouter } from "~/server/api/routers/post";
 import { createTRPCRouter } from "~/server/api/trpc";
 import { signupRouter } from "./routers/signUp";
 
@@ -8,7 +7,6 @@ import { signupRouter } from "./routers/signUp";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   signUp: signupRouter,
 });
 
